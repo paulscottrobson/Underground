20fc824bd8dc928d322fae66ddad6e3be5bce922
Paul Robson <paulscottrobson@gmail.com>
2015-08-08 19:10:29 +0100

    More design and graphics

--- Graphics/makegfx.py
+++ Graphics/makegfx.py
@@ -1,13 +1,24 @@
-#
-#	Build the tilemap tile.
-#
 from PIL import Image
 
+#
+#	Build empty squares by ripping the top layer off the collect graphics
+#
+for i in range(0,10):
+	im = Image.open("empty.png")
+
+	cart = Image.open("collect"+str(i)+".png")
+	cart = cart.crop((0,0,48,12))
+
+	im.paste(cart,(0,32))
+	im.save("empty"+str(i)+".png")
+
+#
+#	Build the 48x48 and 16x16 tile maps
+#
 im = Image.new("RGBA",(48*8,48*5),(0,0,0,0))
 tileCount = 0
 
 def addTile(fileName):
-	print(fileName)
 	global im,tileCount
 	tile = Image.open(fileName)
 	im.paste(tile, ((tileCount % 8)*48,int(tileCount/8)*48))
@@ -29,7 +40,8 @@ addTile("rail.png")
 for i in range(0,6):
 	addTile("block"+str(i)+".png")
 
-
-
-
+# write out at 48x48
 im.save("tiles.png")
+
+# write out at 16x16
+im.resize((int(im.size[0]/3),int(im.size[1]/3))).save("tiles16.png")
